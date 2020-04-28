376ecc55d4297298989a707950cb1ec7f6520a93
Rajesh-Sundaram <ts-rajesh.sundaram@rakuten.com>
2020-04-28 09:51:40 +0900

    bowser integration

--- js-miniapp-sample/src/services/uuid/actions.js
+++ js-miniapp-sample/src/services/uuid/actions.js
@@ -1,28 +1,34 @@
-// import MiniApp from "js-miniapp-sdk";
+import MiniApp from 'js-miniapp-sdk';
 
-import { getUUIDFromMobileSdk } from './../../js_sdk';
-import { SET_UUID /* , UUID_FETCH_ERROR */ } from './types';
+import { getUUIDFromMobileSdk, isMobile } from './../../js_sdk';
+import { SET_UUID, UUID_FETCH_ERROR } from './types';
 
 type GetUUIDAction = { type: String, payload: string };
 
 type UUIDAction = GetUUIDAction;
 
 const setUUID = (): Function => {
-  return dispatch => {
-    const uuid = getUUIDFromMobileSdk();
-    dispatch({
-      type: SET_UUID,
-      payload: uuid,
-    });
-    /* MiniApp.getUniqueId()
-      .then((uuid) => {
-        
-      })
-      .catch((_) => {
-        dispatch({
-          type: UUID_FETCH_ERROR,
+  return (dispatch) => {
+    if (!isMobile()) {
+      const uuid = getUUIDFromMobileSdk();
+      dispatch({
+        type: SET_UUID,
+        payload: uuid,
+      });
+    } else {
+      MiniApp.getUniqueId()
+        .then((uuidFromSDK) => {
+          dispatch({
+            type: SET_UUID,
+            payload: uuidFromSDK,
+          });
+        })
+        .catch((_) => {
+          dispatch({
+            type: UUID_FETCH_ERROR,
+          });
         });
-      }); */
+    }
   };
 };
 
