8b4372a7c2802213e14fda5a8ac190eae7c4a09a
Xavi Majoral <ts-xavier.a.majoral@rakuten.com>
2020-05-25 17:39:12 +0900

    MINIAPP-159_Add_Landing_Page

---
+++ js-miniapp-sample/src/pages/landing.js
@@ -0,0 +1,40 @@
+import React from "react";
+import {Card, CardContent, makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    background: theme.color.secondary,
+    height: 300,
+    maxWidth: 500,
+    width: '95%',
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  content: {
+    height: '25%',
+    justifyContent: 'center',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    fontSize: 18,
+    color: theme.color.primary,
+    fontWeight: 'bold',
+    '& p': {
+      lineHeight: 1.5
+    }
+  },
+}));
+
+const Landing = () => {
+  const classes = useStyles();
+  return (
+    <Card className={classes.root}>
+      <CardContent className={classes.content}>
+        <p>This is a Demo App of Mini App JS SDK</p>
+      </CardContent>
+    </Card>
+  );
+}
+
+export default Landing;
