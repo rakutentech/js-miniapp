d57323cd592c35e40484210bfcbab561fba4dac2
Xavi Majoral <ts-xavier.a.majoral@rakuten.com>
2020-05-27 17:20:43 +0900

    Fix prettier error

--- js-miniapp-sample/src/components/GreyCard.js
+++ js-miniapp-sample/src/components/GreyCard.js
@@ -1,11 +1,11 @@
-import * as React from "react";
+import * as React from 'react';
 
-import {Card, makeStyles} from "@material-ui/core";
+import { Card, makeStyles } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     background: theme.color.secondary,
-    height: props => props.height || 300,
+    height: (props) => props.height || 300,
     maxWidth: 500,
     width: '95%',
   },
@@ -14,16 +14,18 @@ const useStyles = makeStyles((theme) => ({
 type CardType = {
   height?: number | string,
   children: React.Node,
-  className?: string
-}
+  className?: string,
+};
 
 const GreyCard = (props: CardType) => {
   const classes = useStyles(props);
   return (
-    <Card className={`${classes.root} ${props.className ? props.className : ''}`}>
+    <Card
+      className={`${classes.root} ${props.className ? props.className : ''}`}
+    >
       {props.children}
     </Card>
   );
-}
+};
 
 export default GreyCard;
