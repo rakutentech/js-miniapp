74b40d2686bf08a952a7c955a52eb1bfbef494be
Nguyen, Minh | MTSD <ts-minh.nguyen@rakuten.com>
2020-07-02 19:13:52 +0900

    test: request permission

--- js-miniapp-sdk/test/miniapp.spec.ts
+++ js-miniapp-sdk/test/miniapp.spec.ts
@@ -8,20 +8,30 @@ import { MiniAppImp } from '../src/miniapp';
 const window: any = {};
 (global as any).window = window;
 
+window.MiniAppBridge = {
+  getUniqueId: sinon.stub(),
+  requestPermission: sinon.stub(),
+};
+const miniApp = new MiniAppImp();
+
 describe('getUniqueId', () => {
-  before(() => {
-    window.MiniAppBridge = {
-      getUniqueId: sinon.stub(),
-    };
-  });
 
   it('should retrieve the unique id from the Mini App Bridge', () => {
     window.MiniAppBridge.getUniqueId.resolves('test_mini_app_id');
 
-    const miniApp = new MiniAppImp();
-
     return expect(miniApp.getUniqueId()).to.eventually.equal(
       'test_mini_app_id'
+    );
+  });
+});
+
+describe('requestPermission', () => {
+
+  it('should retrieve permission result from the Mini App Bridge', () => {
+    window.MiniAppBridge.requestPermission.resolves('Denied');
+
+    return expect(miniApp.requestPermission('permissionType')).to.eventually.equal(
+      'Denied'
     );
   });
 });
