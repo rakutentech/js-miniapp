2fa980924c7e3aa62e43d15922f1e75decd10c2d
Nguyen, Minh | MTSD <ts-minh.nguyen@rakuten.com>
2020-08-21 12:57:49 +0900

    style: format with prettier

--- js-miniapp-sample/src/pages/media.js
+++ js-miniapp-sample/src/pages/media.js
@@ -1,30 +1,13 @@
 // @flow
 import React from 'react';
-import { Card, CardContent, makeStyles } from '@material-ui/core';
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    height: '100%',
-    width: '100%',
-  },
-  content: {
-    height: '50%',
-    justifyContent: 'space-between',
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    fontSize: 18,
-    color: theme.color.primary,
-    fontWeight: 'bold',
-  },
-}));
 
 const Media = () => {
-  const classes = useStyles();
   return (
     <video width="300" controls>
-      <source src="https://multiplatform-f.akamaihd.net/i/multi/will/bunny/big_buck_bunny_,640x360_400,640x360_700,640x360_1000,950x540_1500,.f4v.csmil/master.m3u8" type="application/x-mpegURL">
-      </source>
+      <source
+        src="https://multiplatform-f.akamaihd.net/i/multi/will/bunny/big_buck_bunny_,640x360_400,640x360_700,640x360_1000,950x540_1500,.f4v.csmil/master.m3u8"
+        type="application/x-mpegURL"
+      ></source>
     </video>
   );
 };
