3133afe5a289b84e160f295d290701ede5b355e5
Nguyen, Minh | MTSD <ts-minh.nguyen@rakuten.com>
2020-08-24 14:37:13 +0900

    refactor: view full width

--- js-miniapp-sample/src/pages/media.js
+++ js-miniapp-sample/src/pages/media.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Media = () => {
   return (
-    <video width="300" controls>
+    <video width="100%" controls>
       <source
         src="https://multiplatform-f.akamaihd.net/i/multi/will/bunny/big_buck_bunny_,640x360_400,640x360_700,640x360_1000,950x540_1500,.f4v.csmil/master.m3u8"
         type="application/x-mpegURL"
