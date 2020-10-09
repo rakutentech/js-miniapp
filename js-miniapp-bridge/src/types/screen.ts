3bb9b05821b60ac063b8243643d237e8d0605920
Nguyen, Minh | MTSD <ts-minh.nguyen@rakuten.com>
2020-10-09 17:31:17 +0900

    refactor: change action name

--- js-miniapp-bridge/src/types/screen.ts
+++ js-miniapp-bridge/src/types/screen.ts
@@ -4,7 +4,7 @@
  * Screen orientation with lock action type.
  */
 export enum ScreenAction {
-  LOCK_PORTRAIT = 'rakuten.miniapp.screen.lock_portrait',
-  LOCK_LANDSCAPE = 'rakuten.miniapp.screen.lock_landscape',
-  LOCK_RELEASE = 'rakuten.miniapp.screen.lock_release',
+  LOCK_PORTRAIT = 'rakuten.miniapp.screen.LOCK_PORTRAIT',
+  LOCK_LANDSCAPE = 'rakuten.miniapp.screen.LOCK_LANDSCAPE',
+  LOCK_RELEASE = 'rakuten.miniapp.screen.LOCK_RELEASE',
 }
