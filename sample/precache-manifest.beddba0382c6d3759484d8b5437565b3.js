44e1dfbe2b6d543c8bd0da11dda6d98461466b33
CI Publisher <dev-opensource@mail.rakuten.com>
2020-12-03 00:33:09 +0000

    sample: Deploy JS Sample App [ci skip]

---
+++ sample/precache-manifest.beddba0382c6d3759484d8b5437565b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c2c65cd0bb49b840299ef7668c2d1e8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f212e4986b4133b6d3a0",
+    "url": "./static/css/main.778ab9ff.chunk.css"
+  },
+  {
+    "revision": "a51c2df07bce253f59b7",
+    "url": "./static/js/2.8626cdc0.chunk.js"
+  },
+  {
+    "revision": "621e67c148318da922fd8fcb225944ab",
+    "url": "./static/js/2.8626cdc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f212e4986b4133b6d3a0",
+    "url": "./static/js/main.8d636cb8.chunk.js"
+  },
+  {
+    "revision": "ad5018763faec8908a97",
+    "url": "./static/js/runtime-main.1c088e3e.js"
+  }
+]);
