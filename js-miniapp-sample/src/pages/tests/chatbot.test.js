1da13cff242a091fb9625b81cfbd00fd6e87a364
minh-rakuten <ts-minh.nguyen@rakuten.com>
2021-02-01 20:00:58 +0900

    test: update chatbot

--- js-miniapp-sample/src/pages/tests/chatbot.test.js
+++ js-miniapp-sample/src/pages/tests/chatbot.test.js
@@ -15,11 +15,12 @@ describe('chatbot', () => {
     renderWithRedux(wrapRouter(wrapTheme(<TalkToChatBot />)));
   });
   test('should load chatbot', () => {
-    expect(screen.getByText('Chatbot')).toBeInTheDocument();
-    expect(screen.getByText('Message')).toBeInTheDocument();
+    expect(screen.getByText('Image')).toBeInTheDocument();
+    expect(screen.getByText('Text')).toBeInTheDocument();
+    expect(screen.getByText('Caption')).toBeInTheDocument();
+    expect(screen.getByText('Action')).toBeInTheDocument();
+    expect(screen.getByText('Title')).toBeInTheDocument();
     expect(screen.getByTestId('send-message')).toBeInTheDocument();
-    expect(screen.getByPlaceholderText('Select Chatbot')).toBeInTheDocument();
-    expect(screen.getByPlaceholderText('Type here...')).toBeInTheDocument();
     expect(screen.queryByTestId('validation-error')).not.toBeInTheDocument();
   });
 
@@ -27,17 +28,9 @@ describe('chatbot', () => {
     userEvent.click(screen.getByTestId('send-message'));
     const validationBlk = screen.queryByTestId('validation-error');
     expect(validationBlk).toBeInTheDocument();
-    expect(validationBlk).toHaveTextContent('enter message to chatbot');
+    expect(validationBlk).toHaveTextContent('image cannot be empty');
     expect(
       screen.queryByTestId('chatbot-response-dialog')
     ).not.toBeInTheDocument();
-  });
-
-  test('should show chatbot response', () => {
-    const messageField = screen.getByPlaceholderText('Type here...');
-    userEvent.type(messageField, 'Hello Rajesh');
-    userEvent.click(screen.getByTestId('send-message'));
-    expect(screen.queryByTestId('validation-error')).not.toBeInTheDocument();
-    expect(screen.getByTestId('chatbot-response-dialog')).toBeInTheDocument();
   });
 });
