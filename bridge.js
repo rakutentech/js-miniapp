ca0b1d30131e0648f8cdd8e8d0d1dba3deb65cc3
CI Publisher <dev-opensource@mail.rakuten.com>
2021-04-06 05:52:04 +0000

    export bridge resource [ci skip]

--- bridge.js
+++ bridge.js
@@ -201,7 +201,7 @@ var MiniAppBridge = /** @class */ (function () {
     };
     /**
      * @param message The message to send to contact.
-     * @returns Promise resolves with the Unique ID which was sent the message.
+     * @returns Promise resolves with the contact id received a message.
      * Can also resolve with empty (undefined) response in the case that the message was not sent to a contact, such as if the user cancelled sending the message.
      * Promise rejects in the case that there was an error.
      */
@@ -209,6 +209,18 @@ var MiniAppBridge = /** @class */ (function () {
         var _this = this;
         return new Promise(function (resolve, reject) {
             return _this.executor.exec('sendMessageToContact', { messageToContact: message }, function (messageId) { return resolve(messageId); }, function (error) { return reject(error); });
+        });
+    };
+    /**
+     * @param id The id of the contact receiving a message.
+     * @param message The message to send to contact.
+     * @returns Promise resolves with the contact id received a message.
+     * @see {sendMessageToContact}
+     */
+    MiniAppBridge.prototype.sendMessageToContactId = function (id, message) {
+        var _this = this;
+        return new Promise(function (resolve, reject) {
+            return _this.executor.exec('sendMessageToContact', { contactId: id, messageToContact: message }, function (messageId) { return resolve(messageId); }, function (error) { return reject(error); });
         });
     };
     return MiniAppBridge;
