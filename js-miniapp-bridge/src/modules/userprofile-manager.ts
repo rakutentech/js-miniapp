80c36777abfc9adf0c1df9d9a057c0c975a34314
Leo <leojoseph.r@rakuten.com>
2024-11-11 15:09:44 +0900

    Prettier

--- js-miniapp-bridge/src/modules/userprofile-manager.ts
+++ js-miniapp-bridge/src/modules/userprofile-manager.ts
@@ -2,28 +2,28 @@ import { MiniAppBridgeUtils, PlatformExecutor } from '../common-bridge';
 import { parseMiniAppError } from '../types/error-types';
 
 export class UserProfileManager {
-    executor: PlatformExecutor;
-    platform: string;
+  executor: PlatformExecutor;
+  platform: string;
 
-    constructor(executor: PlatformExecutor) {
-        this.executor = executor;
-        this.platform = executor.getPlatform();
-    }
+  constructor(executor: PlatformExecutor) {
+    this.executor = executor;
+    this.platform = executor.getPlatform();
+  }
 
-    /**
-     * This interface will be used to know the login status of the User
-     * @see {isLoggedIn}
-     */
-    isLoggedIn() {
-        return new Promise<boolean>((resolve, reject) => {
-            return this.executor.exec(
-                'isLoggedIn',
-                null,
-                response => {
-                    resolve(MiniAppBridgeUtils.BooleanValue(response));
-                },
-                error => reject(parseMiniAppError(error))
-            );
-        });
-    }
+  /**
+   * This interface will be used to know the login status of the User
+   * @see {isLoggedIn}
+   */
+  isLoggedIn() {
+    return new Promise<boolean>((resolve, reject) => {
+      return this.executor.exec(
+        'isLoggedIn',
+        null,
+        response => {
+          resolve(MiniAppBridgeUtils.BooleanValue(response));
+        },
+        error => reject(parseMiniAppError(error))
+      );
+    });
+  }
 }
