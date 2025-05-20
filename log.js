d1df8667b842f78897c9810b4d165700161a03f0
CI Publisher <dev-opensource@mail.rakuten.com>
2025-05-20 11:16:35 +0000

    export bridge/log resource [ci skip]

--- log.js
+++ log.js
@@ -9,11 +9,12 @@ var AndroidSDKLogger = /** @class */ (function () {
     AndroidSDKLogger.prototype.log = function (emoji, type, args) {
         window.MiniAppAndroid.postMessage("".concat(emoji, " console.").concat(type, ": ").concat(Object.values(args)
             .map(function (v) {
-            return typeof v === 'undefined'
-                ? 'undefined'
-                : typeof v === 'object'
-                    ? JSON.stringify(v)
-                    : v.toString();
+            if (typeof v === 'undefined')
+                return 'undefined';
+            else if (typeof v === 'object')
+                return JSON.stringify(v);
+            else
+                return v.toString();
         })
             .map(function (v) { return v.substring(0, 3000); }) // Limit msg to 3000 chars
             .join(', ')));
