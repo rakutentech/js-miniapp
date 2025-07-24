36a4eb030c0ad2b808b5141241c5f18abf7b4f6a
Leo <leojoseph.r@rakuten.com>
2025-07-24 16:54:57 +0900

    Lint

--- js-miniapp-bridge/src/modules/utility-manager.ts
+++ js-miniapp-bridge/src/modules/utility-manager.ts
@@ -91,7 +91,7 @@ export class UtilityManager {
     return new Promise<boolean>((resolve, reject) => {
       return this.executor.exec(
         'launchAppUsingPackageName',
-        { packageName: packageName },
+        { packageName },
         response => {
           resolve(MiniAppBridgeUtils.BooleanValue(response));
         },
