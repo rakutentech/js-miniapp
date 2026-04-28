25e23e4a5e3495133b546943b6be41680f9545fb
Gondi, Tilak | Tilak <tilak.gondi@rakuten.com>
2026-04-28 18:08:28 +0900

    [OAF-33073] [JS SDK] [New Interface] Error handling for isSimInstalled

--- js-miniapp-bridge/src/types/error-types/sim-errors.ts
+++ js-miniapp-bridge/src/types/error-types/sim-errors.ts
@@ -1,26 +1,14 @@
 import { MiniAppError, MiniAppJson } from './mini-app-error';
 
-/**
- * Enum for supported SIM check error event types
- */
 export enum SimCheckErrorType {
   DENIED = 'DENIED',
   FAILED_TO_CHECK = 'FAILED_TO_CHECK',
 }
 
-export interface SimCheckError extends MiniAppJson {
-  code: SimCheckErrorType;
-  message: string;
-}
-
 export class SimCheckError extends MiniAppError {
-  code: SimCheckErrorType;
-
-  constructor(errorInput: MiniAppJson) {
+  constructor(public errorInput: MiniAppJson) {
     super(errorInput);
     Object.setPrototypeOf(this, SimCheckError.prototype);
-    this.code = SimCheckErrorType[errorInput.type];
-    this.message = errorInput.message || `SIM error: ${this.code}`;
   }
 }
 
@@ -29,7 +17,6 @@ export function parseSimError(json: MiniAppJson) {
     SimCheckErrorType[json.type as keyof typeof SimCheckErrorType];
   switch (errorType) {
     case SimCheckErrorType.DENIED:
-      return new SimCheckError(json);
     case SimCheckErrorType.FAILED_TO_CHECK:
       return new SimCheckError(json);
     default:
